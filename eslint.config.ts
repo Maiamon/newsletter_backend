6f100eed6bfff5de38f64f650f22b838b80bbd41
Gabriel Souza Maia <gsmaia23@gmail.com>
2025-09-21 11:13:12 -0400

    Configurações iniciais de projeto

---
+++ eslint.config.ts
@@ -0,0 +1,12 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import json from "@eslint/json";
+import markdown from "@eslint/markdown";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  tseslint.configs.recommended,
+  { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc" },
+  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm" },
+]);
---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import { app } from "./app.ts";
+import { env } from "./env/index.ts";
+
+app.listen({ 
+  host: '0.0.0.0',
+  port: env.PORT 
+}).then(() => {
+  console.log(`HTTP Server is running on http://localhost:${env.PORT}`);
+});
